c16e567589295eef1a6b6c22433c643e5afa8d01
iierickk <erickaue120@gmail.com>
2023-06-23 17:38:13 -0300

    codigo-perfil

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     //se tiver light mode, adicionar a imagme light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/avatar-erick.png")
   } else {
     //se tiver sem light node, manter a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-escuro.png")
   }
 }
